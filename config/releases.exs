498d83fc22a1c4944e736198c31fda5255301bab
Leandro Cesquini Pereira <leandro@leandrocp.com.br>
2019-07-19 14:42:49 -0300

    release heroku

--- config/releases.exs
+++ config/releases.exs
@@ -1,5 +1,1 @@
 import Config
-
-config :live_view_collection, LiveViewCollectionWeb.Endpoint,
-  server: true,
-  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
--- lib/live_view_collection_web/endpoint.ex
+++ lib/live_view_collection_web/endpoint.ex
@@ -2,10 +2,12 @@ defmodule LiveViewCollectionWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :live_view_collection
 
   socket "/socket", LiveViewCollectionWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
-  socket "/live", Phoenix.LiveView.Socket
+  socket "/live", Phoenix.LiveView.Socket,
+    websocket: [timeout: 45_000],
+    longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.
   #
