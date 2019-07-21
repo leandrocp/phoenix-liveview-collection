0b321ee191c8841a85ddd49d7d9470392280b397
Leandro Cesquini Pereira <leandro@leandrocp.com.br>
2019-07-21 14:33:08 -0300

    collection template

---
+++ lib/live_view_collection_web/views/collection_view.ex
@@ -0,0 +1,14 @@
+defmodule LiveViewCollectionWeb.CollectionView do
+  use LiveViewCollectionWeb, :view
+  alias LiveViewCollection.Collection
+
+  def render_tweet(tweet_html) do
+    Phoenix.HTML.raw(tweet_html)
+  end
+
+  def number_of_pages(%{query: query, page_size: page_size}) do
+    collection_length = query |> Collection.filter() |> length()
+    pages = (collection_length / page_size) |> Float.ceil() |> round()
+    if pages <= 1, do: [1], else: 1..pages
+  end
+end
